9c86f604cf166bbc398bd7e1b5a8e75b59ebdc1d
Rudy Guerrero <64393057+SudoRudo@users.noreply.github.com>
2020-07-30 13:50:45 -0400

    Add files via upload

---
+++ monsters-frontend/src/index.js
@@ -0,0 +1,222 @@
+const url = "http://localhost:3000/api/monsters/"
+const docBody = document.querySelector('body')
+const sideBar = document.createElement('div')
+const mainDiv = document.createElement('div')
+sideBar.id = "side-bar"
+mainDiv.className = "main-div"
+docBody.appendChild(sideBar)
+docBody.appendChild(mainDiv)
+
+function sideBarTitle(){
+    const sideTitle = document.createElement('div')
+    sideTitle.innerHTML = `<h3>Monsters</h3>`
+    sideBar.appendChild(sideTitle)
+}
+
+function renderSideBar(monster){
+
+    
+    const head = document.createElement('div')
+    const body = document.createElement('div')
+    const lArm = document.createElement('div')
+    const rArm = document.createElement('div')
+    const legs = document.createElement('div')
+    const name = document.createElement('div')
+    const parentDiv = document.createElement('div')
+    
+    parentDiv.className = "parent-div"
+
+    parentDiv.appendChild(legs)
+    parentDiv.appendChild(rArm)
+    parentDiv.appendChild(body)
+    parentDiv.appendChild(head)
+    parentDiv.appendChild(lArm)
+    parentDiv.appendChild(name)
+
+    head.className = "head" 
+    lArm.className = "lArm"
+    rArm.className = "rArm"
+    legs.className = "legs"
+    body.className = "body"
+    name.className = "name"
+
+    head.innerHTML = `<img src="${monster.head.url}" style="max-width: 20px" />`
+    lArm.innerHTML = `<img src="${monster.left_arm.url}" style="max-width: 10px" />`
+    rArm.innerHTML = `<img src="${monster.right_arm.url}" style="max-width: 10px" />`
+    legs.innerHTML = `<img src="${monster.leg.url}" style="max-width: 23px" />`
+    body.innerHTML = `<img src="${monster.body.url}" style="max-width: 20px" />`
+    name.innerHTML = `<h4>${monster.name}</h4>`
+
+    sideBar.appendChild(parentDiv)
+
+    // parentDiv.addEventListener("click", e => {
+    //     console.log(e.target.closest(".parent-div"))
+    // })
+
+    parentDiv.addEventListener("click", () => {
+        renderMonster(monster);
+        autoFill(monster);
+        let elem = document.querySelector('.submit-div')
+        elem.parentNode.removeChild(elem);
+        updateDelete();
+    })
+}
+
+function renderMonster(monster){
+    mainDiv.innerHTML=""
+
+    const head = document.createElement('div')
+    const body = document.createElement('div')
+    const lArm = document.createElement('div')
+    const rArm = document.createElement('div')
+    const legs = document.createElement('div')
+    const name = document.createElement('div')
+
+    
+    mainDiv.id = monster.id
+    
+
+    mainDiv.appendChild(legs)
+    mainDiv.appendChild(rArm)
+    mainDiv.appendChild(body)
+    mainDiv.appendChild(head)
+    mainDiv.appendChild(lArm)
+    mainDiv.appendChild(name)
+
+    head.id = "head" 
+    lArm.id = "lArm"
+    rArm.id = "rArm"
+    legs.id = "legs"
+    body.id = "body"
+    name.id = "name"
+
+    head.innerHTML = `<img src="${monster.head.url}" />`
+    lArm.innerHTML = `<img src="${monster.left_arm.url}" />`
+    rArm.innerHTML = `<img src="${monster.right_arm.url}" />`
+    legs.innerHTML = `<img src="${monster.leg.url}" />`
+    body.innerHTML = `<img src="${monster.body.url}" />`
+    name.innerHTML = `<h2>${monster.name.toUpperCase()}</h2> `
+
+    
+    
+}
+
+const fetchMonsters=()=>{
+    fetch(url)
+    .then(resp => resp.json())
+    .then(collection => {
+        renderMonsters(collection);
+        submitButton()
+    })
+}
+
+const renderMonsters = monsterObjectArray =>{
+    monsterObjectArray.forEach(monsterObject =>{
+        renderSideBar(monsterObject)
+    })
+    renderMonster(monsterObjectArray[0])
+}
+
+const saveMonster = () =>{
+    document.addEventListener("click", e => {
+        if (e.target.matches('#submit')){
+            const name = document.querySelector('textarea').value
+            const head = parseInt(document.getElementById('head-select').value, 10)
+            const body = parseInt(document.getElementById('body-select').value, 10)
+            const left_arm = parseInt(document.getElementById('left-arm-select').value, 10)
+            const right_arm = parseInt(document.getElementById('right-arm-select').value, 10)
+            const leg = parseInt(document.getElementById('legs-select').value, 10)
+            
+            const monsterObj = {name: name, left_arm_id: left_arm, right_arm_id: right_arm, body_id: body, leg_id: leg, head_id: head, user_id: 1 }
+
+            
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify(monsterObj)
+            })
+            .then(response => response.json())
+            .then( () =>{
+                sideBar.innerHTML = "";
+                fetchMonsters();
+                sideBarTitle()
+            })
+     
+        }
+    })
+}
+
+function autoFill(monster){
+    document.querySelector('textarea').value = monster.name
+    document.getElementById('head-select').value = monster.head.id
+    document.getElementById('body-select').value = monster.body.id
+    document.getElementById('left-arm-select').value = monster.left_arm.id
+    document.getElementById('right-arm-select').value = monster.right_arm.id
+    document.getElementById('legs-select').value = monster.leg.id
+}
+
+const updateMonster = () =>{
+    document.addEventListener("click", e => {
+        if (e.target.matches('#update')){
+            
+            const monId = parseInt(mainDiv.id, 10)
+
+            const name = document.querySelector('textarea').value
+            const head = parseInt(document.getElementById('head-select').value, 10)
+            const body = parseInt(document.getElementById('body-select').value, 10)
+            const left_arm = parseInt(document.getElementById('left-arm-select').value, 10)
+            const right_arm = parseInt(document.getElementById('right-arm-select').value, 10)
+            const leg = parseInt(document.getElementById('legs-select').value, 10)
+            
+            const monsterObj = {name: name, left_arm_id: left_arm, right_arm_id: right_arm, body_id: body, leg_id: leg, head_id: head, user_id: 1 }
+
+            
+            fetch(`http://localhost:3000/api/monsters/${monId}`, {
+                method: "PATCH",
+                headers: {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify(monsterObj)
+            })
+            .then(response => response.json())
+            .then( () =>{
+                sideBar.innerHTML = "";
+                fetchMonsters();
+                sideBarTitle()
+            })
+     
+        }
+    })
+}
+
+const deleteMonster = () => {
+    document.addEventListener("click", e => {
+        if (e.target.matches('#delete')){
+            const monId = parseInt(mainDiv.id, 10)
+            const monName = document.getElementById('name').innerText
+                var r = confirm(`Are you sure you want to delete ${monName}`);
+                if (r == true) {
+                  fetch(`http://localhost:3000/api/monsters/${monId}`, {
+                      method: 'DELETE'
+                  })
+                  .then( ()=>{
+                    sideBar.innerHTML = "";
+                    fetchMonsters();
+                    sideBarTitle()
+                })
+                } else {
+                }
+        }
+    })
+} 
+
+
+fetchMonsters()
+saveMonster()
+updateMonster()
+deleteMonster()
+sideBarTitle()
---
+++ monsters-frontend/src/partsSelect.js
@@ -0,0 +1,234 @@
+
+const body = document.querySelector('body')
+const dropDownGrandfather = document.createElement('div')
+const dropDownDiv = document.createElement('div')
+dropDownGrandfather.classList += "grand-div"
+dropDownDiv.classList += "drop-down-div"
+
+body.appendChild(dropDownGrandfather)
+dropDownGrandfather.appendChild(dropDownDiv)
+
+function headSelect(){
+    function renderHeadMenu(heads){
+        const headMenuDiv = document.createElement('div')
+        headMenuDiv.classList += "menu-div"
+        
+        headMenuDiv.innerHTML = `
+        <label for="heads">Head</label>
+        <select name="heads" id="head-select">
+        </select>
+        `
+        const selectEl = headMenuDiv.querySelector('select')
+    
+        
+        heads.forEach(head => {
+            selectEl.innerHTML += `<option value="${head.id}" >${head.name}</option>`;
+        });
+        
+        selectEl.addEventListener("change", e => {
+            const headPart = document.querySelector('#head');
+            headPart.innerHTML = `<img src="${heads[e.target.value - 1].url}" />`;
+            console.log(e.target.value)
+    
+        });
+    
+    
+        dropDownDiv.appendChild(headMenuDiv)
+        console.log(selectEl)
+    
+    }
+    
+    fetch('http://localhost:3000/heads')
+    .then(res => res.json())
+    .then(headsArray => {
+        renderHeadMenu(headsArray)
+    })
+}
+
+function bodySelect(){
+    function renderBodyMenu(bodies){
+        const bodyMenuDiv = document.createElement('div')
+        bodyMenuDiv.classList += "menu-div"
+        
+        bodyMenuDiv.innerHTML = `
+        <label for="bodies">Body</label>
+        <select name="bodies" id="body-select">
+        </select>
+        `
+        const selectEl = bodyMenuDiv.querySelector('select')
+    
+        
+        bodies.forEach(body => {
+            selectEl.innerHTML += `<option value="${body.id}" >${body.name}</option>`;
+        });
+    
+        selectEl.addEventListener("change", e => {
+            const bodyPart = document.querySelector('#body');
+            bodyPart.innerHTML = `<img src="${bodies[e.target.value - 1].url}" />`;
+            console.log(e.target.value)
+    
+        });
+    
+    
+    
+        dropDownDiv.appendChild(bodyMenuDiv)
+        console.log(selectEl)
+    }
+    
+    fetch('http://localhost:3000/bodies')
+    .then(res => res.json())
+    .then(bodiesArray => {
+        renderBodyMenu(bodiesArray)
+    })
+}
+
+function rightArmSelect(){
+    function renderRightArmMenu(rightArms){
+        const rightArmMenuDiv = document.createElement('div')
+        rightArmMenuDiv.classList += "menu-div"
+        
+        rightArmMenuDiv.innerHTML = `
+        <label for="rightArms">Right Arm</label>
+        <select name="right-arms" id="right-arm-select">
+        </select>
+        `
+        const selectEl = rightArmMenuDiv.querySelector('select')
+    
+        
+        rightArms.forEach(rightArm => {
+            selectEl.innerHTML += `<option value="${rightArm.id}" >${rightArm.name}</option>`;
+        });
+        
+        selectEl.addEventListener("change", e => {
+            const rightArmPart = document.querySelector('#rArm');
+            rightArmPart.innerHTML = `<img src="${rightArms[e.target.value - 1].url}" />`;
+            console.log(e.target.value)
+    
+        });
+    
+        dropDownDiv.appendChild(rightArmMenuDiv)
+        console.log(selectEl)
+    }
+    
+    fetch('http://localhost:3000/right_arms')
+    .then(res => res.json())
+    .then(rightArmsArray => {
+        renderRightArmMenu(rightArmsArray)
+    })
+}
+
+
+function leftArmSelect(){
+    function renderLeftArmMenu(leftArms){
+        const leftArmMenuDiv = document.createElement('div')
+        leftArmMenuDiv.classList += "menu-div"
+        
+        leftArmMenuDiv.innerHTML = `
+        <label for="leftArms">Left Arm</label>
+        <select name="left-arms" id="left-arm-select">
+        </select>
+        `
+        const selectEl = leftArmMenuDiv.querySelector('select')
+    
+        
+        leftArms.forEach(leftArm => {
+            selectEl.innerHTML += `<option value="${leftArm.id}" >${leftArm.name}</option>`;
+        });
+        
+        selectEl.addEventListener("change", e => {
+            const leftArmPart = document.querySelector('#lArm');
+            leftArmPart.innerHTML = `<img src="${leftArms[e.target.value - 1].url}" />`;
+            console.log(e.target.value)
+    
+        });
+    
+        dropDownDiv.appendChild(leftArmMenuDiv)
+        console.log(selectEl)
+    }
+    
+    fetch('http://localhost:3000/left_arms')
+    .then(res => res.json())
+    .then(leftArmsArray => {
+        renderLeftArmMenu(leftArmsArray)
+    })
+}
+
+function legSelect(){
+    function renderLegMenu(legs){
+        const legMenuDiv = document.createElement('div')
+        legMenuDiv.classList += "menu-div"
+        
+        legMenuDiv.innerHTML = `
+        <label for="legs">Legs</label>
+        <select name="legs" id="legs-select">
+        </select>
+        `
+        const selectEl = legMenuDiv.querySelector('select')
+    
+        
+        legs.forEach(leg => {
+            selectEl.innerHTML += `<option value="${leg.id}" >${leg.name}</option>`;
+        });
+        
+        selectEl.addEventListener("change", e => {
+            const legPart = document.querySelector('#legs');
+            legPart.innerHTML = `<img src="${legs[e.target.value - 1].url}" />`;
+            console.log(e.target.value)
+    
+        });
+    
+        dropDownDiv.appendChild(legMenuDiv)
+        console.log(selectEl)
+    }
+    
+    fetch('http://localhost:3000/legs')
+    .then(res => res.json())
+    .then(legsArray => {
+        renderLegMenu(legsArray)
+    })
+}
+
+function nameSelect(){
+    const nameDiv = document.createElement('div')
+    nameDiv.classList += "menu-div"
+    dropDownDiv.appendChild(nameDiv)
+    nameDiv.innerHTML = `
+    <label for="name">Name Your Little Guy</label><br>
+    <textarea id="name" name="name" rows="1" ></textarea>`
+}
+
+function submitButton(){
+    const submitDiv = document.createElement('div')
+    submitDiv.classList += "submit-div"
+    docBody.append(submitDiv)
+    submitDiv.innerHTML = `<button type="button" id="submit">Submit!</button>`
+}
+
+function updateDelete(){
+    const updateDeleteDiv = document.createElement('div')
+    updateDeleteDiv.className = "update-delete"
+    docBody.append(updateDeleteDiv)
+
+    const updateSpan = document.createElement('span')
+    updateDeleteDiv.append(updateSpan)
+    updateSpan.innerHTML = `<button type="button" id="update">Update!</button> || `
+
+    const deleteSpan = document.createElement('span')
+    deleteSpan.classList += "delete-span"
+    updateDeleteDiv.append(deleteSpan)
+    deleteSpan.innerHTML = `<button type="button" id="delete">Delete Monster</button>`
+
+}
+
+headSelect()
+bodySelect()
+legSelect()
+rightArmSelect()
+leftArmSelect()
+nameSelect()
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+})
