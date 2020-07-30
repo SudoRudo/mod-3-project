adc74e332a8ee7681bc4b6036e032aaf78a53210
Rudy Guerrero <64393057+SudoRudo@users.noreply.github.com>
2020-07-30 13:55:58 -0400

    Add files via upload

---
+++ monsters-api/test/controllers/monster_parts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MonsterPartsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ monsters-api/test/controllers/right_arms_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RightArmsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
